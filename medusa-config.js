59497389445a230dfb754e4bb3c3f11b6261bb7c
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2024-03-10 12:44:19 -0700

    vercel update medusaconfig.js

--- medusa-config.js
+++ medusa-config.js
@@ -23,10 +23,11 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,/vercel\.app$/";
+
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "/vercel\.app$/";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
