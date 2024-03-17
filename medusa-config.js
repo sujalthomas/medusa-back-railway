6cc6b772a4dc81c0b802b39463b7024b296b2767
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2024-03-17 15:25:27 -0700

    yeee

--- medusa-config.js
+++ medusa-config.js
@@ -56,7 +56,7 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -67,7 +67,7 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
